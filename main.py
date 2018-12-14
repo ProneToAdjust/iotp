3c791bf0245da6ad7db10748ee47b4361ce11bde
Ryan Raspi <ryanong14@gmail.com>
2018-12-14 12:21:04 +0000

    commit

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import bme280
+import time
+import paho.mqtt.client as mqtt
+import json
+
+mqtt_broker = "m2m.eclipse.org"
+topic = "iotp/tph"
  
-temperature,pressure,humidity = bme280.readBME280All()
+while True:
  
-print "Temperature : ", temperature, "C"
-print "Pressure : ", pressure, "hPa"
-print "Humidity : ", humidity, "%"
+	# temperature,pressure,humidity = bme280.readBME280All()
+	tph = bme280.readBME280All()
+	 
+	#print (tph)
+	
+	payload = json.dumps(tph)
+	
+	my_mqtt = mqtt.Client()
+	print("\nCreated client object at "+ time.strftime("%H:%M:%S"))
+	
+	my_mqtt.connect(mqtt_broker, port=1883)
+	print("--connected to broker")
+	
+	try:
+		#my_mqtt.publish(topic, pay_load)
+		my_mqtt.publish(topic,payload)
+		print(payload)
+		
+		time.sleep(1)
+		
+	except:
+		print("--error publishing!")
+		
+	else:
+		my_mqtt.disconnect()
+		print("--disconnected from broker")
--- mqtt_subscriber.py
+++ mqtt_subscriber.py
@@ -1,29 +1,27 @@
 import time
+import json
 import paho.mqtt.client as mqtt
-import psutil
+
 mqtt_broker = "m2m.eclipse.org"
-topic_cpu = "tp/eng/iotp_lab/pc_010101/cpu"
-topic_vmem = "tp/eng/iotp_lab/pc_010101/virtual_mem"
+topic_cpu = "iotp/tph"
 my_mqtt = None
+
 def onMessage(client, userdata, message):
-	print("%s %s" % (message.topic, message.payload))
+	#print("%s %s" % (message.topic, message.payload))
+	m_decode=str(message.payload.decode("utf-8","ignore"))
+	tph = json.loads(m_decode)
+	for x in tph:
+		print (x)
 
 def startMQTT():
 	my_mqtt = mqtt.Client()
 	my_mqtt.on_message = onMessage
 	my_mqtt.connect(mqtt_broker, port=1883)
 	my_mqtt.subscribe(topic_cpu, qos=1)
-	my_mqtt.subscribe(topic_vmem, qos=1)
-	my_mqtt.loop_start()
-	print("Subscribed to topic1")
-	
-	#my_mqtt = mqtt.Client()
-	#my_mqtt.on_message = onMessage
-	#my_mqtt.connect(mqtt_broker, port=1883)
 	#my_mqtt.subscribe(topic_vmem, qos=1)
-	#my_mqtt.loop_start()
-	print("Subscribed to topic2")
-
+	my_mqtt.loop_start()
+	print("Subscribed to topic")
+	
 def main():
 	startMQTT()
 	while True:
