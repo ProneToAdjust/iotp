66e71f3f5e35079ace76f162f1f3147f46f8f089
Ryan Raspi <ryanong14@gmail.com>
2019-01-18 03:35:21 +0000

    commit

--- G1.py
+++ G1.py
@@ -1,32 +1,55 @@
 import RPi.GPIO as G
 import sys
 import time as t
-
-# get and type cast second argument to int
-first_arg = int(sys.argv[1])
+import threading
 
 # set up gpio
 G.setmode(G.BCM)
 G.setwarnings(False)
 G.setup(27,G.OUT)
-p = G.PWM(27, 1000)
+p = G.PWM(27, 500)
+p.start(0)
 
-print(first_arg)
+f= open("var.txt","w+")
+f.write("1")
+f.close()
+globalVal = 1
+brightnessVar = 1
 
-# if first_arg is one, on led
-if(first_arg == 1):
-	p.start(100)
-	while True:
-		continue
-	
-# if zero, off led
-elif(first_arg == 0):
-	p.start(0)
-	while True:
-		continue
-	
-# if three, exit python script and clean up
-elif(first_arg == 3):
-	G.cleanup()
-	print("Exited")
-	exit()
+def foo():
+	threading.Timer(0.1, foo).start()
+	global brightnessVar
+	f= open("var.txt","r")
+	brightnessVar =int(f.read())
+	setValue(brightnessVar)
+
+def setValue(val):
+    global globalVal
+    valueChanged= globalVal != val
+    if valueChanged:
+        changeBrightness()
+        globalVal = val
+		
+def changeBrightness():
+	if(brightnessVar == 1):
+		p.ChangeDutyCycle(25)
+		
+	elif(brightnessVar == 2):
+		p.ChangeDutyCycle(50)
+		
+	elif(brightnessVar == 3):
+		p.ChangeDutyCycle(100)
+		
+	elif(brightnessVar == 4):
+		p.stop()
+		G.cleanup()
+		f= open("var.txt","w")
+		f.write("1")
+		f.close()
+		print("Exited")
+		exit()
+foo()
+
+while True:
+	t.sleep(0.1)
+	continue
--- index1.php
+++ index1.php
@@ -7,6 +7,8 @@
 
 </FORM>
 
+<?php echo "Brightness Control"?>
+
 <FORM action="<?=$_SERVER['PHP_SELF']?>" method = "post">
 
 <input type="submit" name="submitted" value="LOW"/>
@@ -15,46 +17,38 @@
 
 </FORM>
 
-<FORM action="<?=$_SERVER['PHP_SELF']?>" method = "post">
-
-<input type="submit" name="submitted" value="EXIT"/>
-
-</FORM>
-
 <?php
 
 	switch($_POST['submitted'])
 	{
 		case "ON":
-		$OFFpython = "^C";
+		$OFFpython = "sudo python /var/www/html/G1.py > /dev/null &";
 		$cmd = exec($OFFpython);
-		$OFFpython = "sudo python /var/www/html/G1.py 1";
-		$cmd = exec($OFFpython);
-		echo "<pre>$cmd</pre>";
-		break;
-		
-		case "OFF":
-		$OFFpython = "^C";
-		$cmd = exec($OFFpython);
-		$OFFpython = "sudo python /var/www/html/G1.py 0";
-		$cmd = exec($OFFpython);
-		echo "<pre>$cmd</pre>";
+		echo "ON";
 		break;
 		
 		case "LOW":
+		$OFFpython = "sudo python /var/www/html/setTextFile.py 1";
+		$cmd = exec($OFFpython);
 		echo "LOW";
 		break;
 		
 		case "MED":
+		$OFFpython = "sudo python /var/www/html/setTextFile.py 2";
+		$cmd = exec($OFFpython);
+		echo "MED";
 		break;
 		
 		case "HIGH":
+		$OFFpython = "sudo python /var/www/html/setTextFile.py 3";
+		$cmd = exec($OFFpython);
+		echo "HIGH";
 		break;
 		
-		case "EXIT":
-		$OFFpython = "sudo python /var/www/html/G1.py 3";
+		case "OFF":
+		$OFFpython = "sudo python /var/www/html/setTextFile.py 4";
 		$cmd = exec($OFFpython);
-		echo "<pre>$cmd</pre>";
+		echo "OFF";
 		break;
 		
 				}
